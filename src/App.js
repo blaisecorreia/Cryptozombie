7ba34db47176a559bea33288b95ffcf4a7b11bdd
BLAISE <student5@MacBook-Pro-378.lan>
2025-10-18 15:29:46 -0700

    Final commit - Truffle Blockchain Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🧟 Welcome to the CryptoZombie DApp</h1>
+      <p>Metamask connected? Time to create your zombie army!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const ZOMBIE_OWNERSHIP_CONTRACT_ADDRESS = "0xa7817F1437dB1d28d4C29Bdf2c578945fB3eaF9F";
+export const ZOMBIE_FEEDING_CONTRACT_ADDRESS = "0x8389143a1d4f8d7E4A471eA2e1506271B457d2A9";
+export const ETHEREUM_NETWORK = "http://localhost:7545";
+export const CRYPTO_KITTIES = "https://api.cryptokitties.co/kitties?limit=8";
